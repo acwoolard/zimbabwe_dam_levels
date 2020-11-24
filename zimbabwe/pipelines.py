ebdda1e6cb4cc838488243159b6eb1b4539cab77
Andrew Woolard <andrew.c.woolard@gmail.com>
2020-11-24 15:58:45 +0200

    Added pipeline for processing

--- zimbabwe/pipelines.py
+++ zimbabwe/pipelines.py
@@ -6,8 +6,36 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import os.path
 
 
 class ZimbabwePipeline:
     def process_item(self, item, spider):
+
+        adapter = ItemAdapter(item)
+
+        coords_dict = {}
+
+        current_directory = os.path.dirname(__file__)
+        parent = os.path.split(current_directory)[0]
+        dict_path = os.path.join(parent,"dam_coords.txt")
+
+        f = open(dict_path,mode='r')
+
+        line = f.readline()
+        while line:
+            coords_dict[line.split(",")[0]]=[line.split(",")[1],line.split(",")[2]]
+            line = f.readline()
+    
+        f.close()
+
+        lon = 0
+        lat = 0
+        if adapter['name'] in coords_dict.keys():
+            lon = coords_dict[adapter['name']][0]
+            lat = coords_dict[adapter['name']][1][:-1]
+
+        adapter["longitude"]=lon
+        adapter["latitude"]=lat
+
         return item
--- zimbabwe/spiders/zimbabwe_spider.py
+++ zimbabwe/spiders/zimbabwe_spider.py
@@ -1,5 +1,4 @@
 import scrapy
-import os.path
 
 class zimbabweSpider(scrapy.Spider):
     name = 'zimbabwe'
@@ -7,33 +6,12 @@ class zimbabweSpider(scrapy.Spider):
 
     def parse(self, response):
 	
-        coords_dict = {}
-
-        current_directory = os.path.dirname(__file__)
-        parent1 = os.path.split(current_directory)[0]
-        parent2 = os.path.split(parent1)[0]
-        dict_path = os.path.join(parent2,"dam_coords.txt")
-
-        f = open(dict_path,mode='r')
-
-        line = f.readline()
-        while line:
-            coords_dict[line.split(",")[0]]=[line.split(",")[1],line.split(",")[2]]
-            line = f.readline()
-    
-        f.close()
 
         for row in response.xpath('//*[@class="jtTableContainer jtrespo-scroll  "]//tbody/tr')[1:]:
             name=row.xpath('td[1]//text()').extract_first()
-            lon = 0
-            lat = 0
-            if name in coords_dict.keys():
-                lon = coords_dict[name][0]
-                lat = coords_dict[name][1][:-1]
+            
             yield {
                 'name' : name,
-                'longitude' : lon,
-                'latitude' : lat,
                 'full': row.xpath('td[2]//text()').extract_first(),
                 'current' : row.xpath('td[3]//text()').extract_first(),
 	    }
